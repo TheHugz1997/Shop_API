d4e07bb2c7c5f5bdc8f7031847a7956c421aaba5
Hugo Dubois <195347@ecam.be>
2023-12-31 14:52:30 +0100

    Adding communication example of interfaces with other microservices

--- server.js
+++ server.js
@@ -56,3 +56,52 @@ client.connect()
     .catch((err) => {
         console.error('Error connecting to Cassandra:', err);
 });
+
+/*
+    *********************************************
+    ****COMMUNCATION WITH OTHER MICROSERVICES****
+    *********************************************
+*/
+
+// // Function to make requests to other microservices
+// async function makeMicroserviceRequest(microservice, endpoint, method = 'GET', data = {}) {
+//     const microservicePortMap = {
+//         stock: 30015,
+//         users: 30019,
+//         payment: 30021,
+//     };
+
+//     const microservicePort = microservicePortMap[microservice];
+
+//     const url = `http://localhost:${microservicePort}/${endpoint}`;
+
+//     try {
+//         const response = await axios({
+//             method,
+//             url,
+//             data,
+//         });
+
+//         return response.data;
+//     } catch (error) {
+//         console.error(`Error making ${method} request to ${microservice} microservice:`, error.message);
+//         throw error;
+//     }
+// }
+
+// // Example usage:
+// app.get('/getStockData', async (req, res) => {
+//     try {
+//         const stockData = await makeMicroserviceRequest('stock', 'getStockData');
+//         res.json(stockData);
+//     } catch (error) {
+//         res.status(500).json({ error: 'Internal Server Error' });
+//     }
+// });
+
+// // ... (define similar routes for other microservices)
+
+// // Start the server
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
